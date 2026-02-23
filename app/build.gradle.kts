9d56168310b3877343182f2dfbdfbd7cf38af3c5
mi <mi@ligang19.local>
2026-02-23 17:21:54 +0800

    fix: Update compileSdk and targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.recordingapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.recordingapp"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
